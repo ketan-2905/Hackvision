01fc285c4f119dc9bcd29777ff1fb7538f1f3836
ketan-2905 <ketangaikwad2905@gmail.com>
2026-01-22 11:33:28 +0530

    Setup is done

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/",(req,res) => {
+   res.send("Hi there")
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}/`);
+});
